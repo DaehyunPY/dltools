e774c6530081346232a75dad9d1822fbe5620e3e
Daehyun You <daehyun.park.you@gmail.com>
2018-12-04 13:48:42 +0900

    Format build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-
 dir=$(realpath "$(dirname $0)")
 
 docker run \
-    --rm \
-    --interactive \
-    --volume "$dir":/app \
-    --workdir /app \
-    quay.io/pypa/manylinux1_x86_64 \
-    bash <<EOF
-
-for PY in /opt/python/cp37-cp37m/bin/python \
-          /opt/python/cp36-cp36m/bin/python \
-          /opt/python/cp35-cp35m/bin/python
-do
-    \$PY -m pip install pybind11 cython numpy
-    \$PY setup.py sdist bdist_wheel
-done
-ls dist/*.whl | xargs -i auditwheel repair {}
-
+	--rm \
+	--interactive \
+	--volume "$dir":/app \
+	--workdir /app \
+	quay.io/pypa/manylinux1_x86_64 bash <<EOF
+        for py in \
+            /opt/python/cp37-cp37m/bin/python \
+            /opt/python/cp36-cp36m/bin/python \
+            /opt/python/cp35-cp35m/bin/python
+        do
+            \$py -m pip install pybind11 cython numpy
+            \$py setup.py sdist bdist_wheel
+        done
+        ls dist/*.whl | xargs -i auditwheel repair {}
 EOF
