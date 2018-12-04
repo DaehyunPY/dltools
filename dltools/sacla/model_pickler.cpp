0cbc5d95d0599f331c71c4d548c951de32626b34
Daehyun You <daehyun.park.you@gmail.com>
2018-12-04 20:14:32 +0900

    Format dltools/sacla/model_pickler.cpp

--- dltools/sacla/model_pickler.cpp
+++ dltools/sacla/model_pickler.cpp
@@ -11,22 +11,27 @@ using std::unordered_map;
 pybind11::dict dltools::sacla::_model__getstate__(const dltools::sacla::Model &m)
 {
     return pybind11::dict(
-        "mass"_a = m._mass(), "pz_coeffs"_a = m._pz_coeffs(), "pr_coeffs"_a = m._pr_coeffs(),
+        "mass"_a = m._mass(),
+        "pz_coeffs"_a = m._pz_coeffs(),
+        "pr_coeffs"_a = m._pr_coeffs(),
         "fr"_a = m._fr(), "to"_a = m._to(),
         "t0"_a = m._t0(), "th"_a = m._th(),
-        "x0"_a = m._x0(), "y0"_a = m._y0(), "dx"_a = m._dx(), "dy"_a = m._dy());
+        "x0"_a = m._x0(), "y0"_a = m._y0(),
+        "dx"_a = m._dx(), "dy"_a = m._dy()
+    );
 }
 
 dltools::sacla::Model dltools::sacla::_model__setstate__(pybind11::dict d)
 {
-    return Model(
+    return {
         d["mass"].cast<double>(),
         d["pz_coeffs"].cast<std::array<double, 7>>(),
         d["pr_coeffs"].cast<std::array<double, 6>>(),
         d["fr"].cast<double>(), d["to"].cast<double>(),
         d["t0"].cast<double>(), d["th"].cast<double>(),
         d["x0"].cast<double>(), d["y0"].cast<double>(),
-        d["dx"].cast<double>(), d["dy"].cast<double>());
+        d["dx"].cast<double>(), d["dy"].cast<double>()
+    };
 }
 
 pybind11::dict dltools::sacla::_models__getstate__(const dltools::sacla::Models &m)
