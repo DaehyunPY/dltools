862d0bcb89c584c7f4ed68d3ffb354ea45970e9c
Daehyun You <daehyun.park.you@gmail.com>
2019-01-12 11:56:57 +0900

    Update version

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ ext_modules = [
 
 setup(
     name='dltools',
-    version='v201812.1',
+    version='v201901.0',
     author='Daehyun You',
     author_email='daehyun@dc.tohoku.ac.jp',
     url='https://github.com/DaehyunPY/dltools',
