765aca8e4494f2a7402e1077440be6a66f128cc3
Daehyun You <daehyun.park.you@gmail.com>
2019-01-12 11:44:40 +0900

    Add module dltools.cov.generic

--- dltools/cov/__init__.py
+++ dltools/cov/__init__.py
@@ -1,8 +1,10 @@
 from .simple import *
 from .inapool import *
+from .generic import *
 
 
 __all__ = [
     "cov11_simple", "cov111_simple",
     "cov11_inapool", "cov21_inapool", "cov111_inapool", "cov211_inapool",
+    "cov11_generic", "cov21_generic", "cov111_generic", "cov211_generic",
 ]
