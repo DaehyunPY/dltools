9f44a096fb4ecf8b8e086946951085f1458fc3b5
Daehyun You <daehyun.park.you@gmail.com>
2019-01-12 11:55:26 +0900

    Add module dltools.cov.inpools

--- dltools/cov/__init__.py
+++ dltools/cov/__init__.py
@@ -1,10 +1,12 @@
 from .simple import *
 from .inapool import *
+from .inpools import *
 from .generic import *
 
 
 __all__ = [
     "cov11_simple", "cov111_simple",
     "cov11_inapool", "cov21_inapool", "cov111_inapool", "cov211_inapool",
+    "cov1x11_inpools", "cov2x11_inpools",
     "cov11_generic", "cov21_generic", "cov111_generic", "cov211_generic",
 ]
